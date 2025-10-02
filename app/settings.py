05966c518ec3aea8cc780f50d1b077146ead6efe
guizinnn12 <gui.andrade.corp@gmail.com>
2025-10-02 13:47:07 -0300

    registro de login

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cars',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
-MEDIA_URL= '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
