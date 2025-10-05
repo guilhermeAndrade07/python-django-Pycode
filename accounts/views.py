276c45b223a5e8d053d322f71751e449e3e5bc0e
guizinnn12 <gui.andrade.corp@gmail.com>
2025-10-05 00:51:31 -0300

    Aplicando em metade do projeto Class Based View
    Trabalhando com ListView, CreateView w DetailView, facilitando e deixando o código mais limpo

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.views import View
 
 
-def register_view(request):
-    if request.method == "POST":
+class RegisterView(View):
+
+    def get(self, request):
+        user_form = UserCreationForm()
+        return render(request, 'register.html', {'user_form': user_form})
+
+    def post(self, request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
             return redirect('login')
-    else:
-        user_form = UserCreationForm()
-    return render(request, 'register.html', {'user_form': user_form})
+        return render(request, 'register.html', {'user_form': user_form})
 
 
-def login_view(request):
-    if request.method == "POST":
+class LoginView(View):
+
+    def get(self, request):
+        login_form = AuthenticationForm()
+        return render(request, 'login.html', {'login_form': login_form})
+
+    def post(self, request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
@@ -24,9 +33,7 @@ def login_view(request):
             return redirect('cars_list')
         else:
             login_form = AuthenticationForm()
-    else:
-        login_form = AuthenticationForm()
-    return render(request, 'login.html', {'login_form': login_form})
+        return render(request, 'login.html', {'login_form': login_form})
 
 
 def logout_view(request):
