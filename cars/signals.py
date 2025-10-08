d3980efe3dc2d5b382b09bb04165e5bdc0ae6cb9
guizinnn12 <gui.andrade.corp@gmail.com>
2025-10-08 13:05:16 -0300

    integrando api da OpenAI

--- cars/signals.py
+++ cars/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
@@ -13,15 +14,22 @@ def car_inventory_update():
         cars_count=cars_count,
         cars_value=cars_value
     )
-    
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs_save):
+    if not instance.bio:
+        ai_bio = get_car_ai_bio(
+            instance.model, instance.brand, instance.model_year
+        )
+        instance.bio = ai_bio
 
 
 @receiver(post_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
+def car_post_save(sender, instance, **kwargs):
     car_inventory_update()
 
 
 @receiver(post_delete, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
+def car_post_delete(sender, instance, **kwargs):
     car_inventory_update()
-
