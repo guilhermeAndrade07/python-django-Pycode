893ae3faac696b42380f72adcb6f7395b8729e0a
guizinnn12 <gui.andrade.corp@gmail.com>
2025-10-15 10:07:48 -0300

    add key for open ai

--- openai_api/client.py
+++ openai_api/client.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_KEY= "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"
 
 client = OpenAI(
     api_key=OPENAI_API_KEY
